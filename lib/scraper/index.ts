d0b5074ae512bf317fa657b1ff9fd7020bec4cba
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-02-24 00:12:02 +0530

    Add new functionalities and files

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,39 +1,94 @@
 import axios from "axios";
-import * as cheerio from 'cheerio';
+import * as cheerio from "cheerio";
+import { extractCurrency, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
-    if(!url) return;
+  if (!url) return;
 
-    // curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_b1f2253c-zone-pricewise:kx5un7pt9o2u -k https://lumtest.com/myip.json
+  // curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_b1f2253c-zone-pricewise:kx5un7pt9o2u -k https://lumtest.com/myip.json
 
-    // BrightData proxy configuration
-    const username = String(process.env.BRIGHT_DATA_USERNAME);
-    const password = String(process.env.BRIGHT_DATA_PASSWORD);
-    const port = 22225;
-    const session_id = (1000000 * Math.random()) | 0;
-    const options = { 
-        auth: {
-            username: `${username}-session-${session_id}`,
-            password,
-        },
-        host: 'brd.superproxy.io',
-        port,
-        rejectUnauthorized: false,
-    }
-    // options object above
-    // with this option object we can request data from BrightData
+  // BrightData proxy configuration
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: "brd.superproxy.io",
+    port,
+    rejectUnauthorized: false,
+  };
+  // options object above
+  // with this option object we can request data from BrightData
 
-    try {
-        // Fetch the product page
-        const response = await axios.get(url, options);
-        const $ = cheerio.load(response.data);
+  try {
+    // Fetch the product page
+    const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
 
-        // Extract the product title
-        const title = $('#productTitle').text().trim();
+    // Extract the product title
+    const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
 
-        console.log({title});
-        
-    } catch (error: any) {
-        throw new Error(`Failed to scrape product: ${error.message}`)
-    }
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+
+    const imageUrls = Object.keys(JSON.parse(images));
+    // after getting the images we are parsing it
+
+    const currency = extractCurrency($(".a-price-symbol"));
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+
+    console.log({
+      title,
+      currentPrice,
+      originalPrice,
+      outOfStock,
+      imageUrls,
+      currency,
+      discountRate,
+    });
+
+    // Construct data object with scraped information
+    // {instead of console.log, we form an object of it}
+
+    const data = {
+      url,
+      currency: currency || "$",
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice),
+      originalPrice: Number(originalPrice),
+    //   Number(...) --> turning it to a number
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: "category",
+      reviewsCount: 100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+    };
+  } catch (error: any) {
+    throw new Error(`Failed to scrape product: ${error.message}`);
+  }
 }
