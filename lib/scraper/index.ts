2bdffc407060157c54a5f4cf749286afd6c502fd
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-02-23 20:40:38 +0530

    first commit

---
+++ lib/scraper/index.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import * as cheerio from 'cheerio';
+
+export async function scrapeAmazonProduct(url: string) {
+    if(!url) return;
+
+    // curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_b1f2253c-zone-pricewise:kx5un7pt9o2u -k https://lumtest.com/myip.json
+
+    // BrightData proxy configuration
+    const username = String(process.env.BRIGHT_DATA_USERNAME);
+    const password = String(process.env.BRIGHT_DATA_PASSWORD);
+    const port = 22225;
+    const session_id = (1000000 * Math.random()) | 0;
+    const options = { 
+        auth: {
+            username: `${username}-session-${session_id}`,
+            password,
+        },
+        host: 'brd.superproxy.io',
+        port,
+        rejectUnauthorized: false,
+    }
+    // options object above
+    // with this option object we can request data from BrightData
+
+    try {
+        // Fetch the product page
+        const response = await axios.get(url, options);
+        const $ = cheerio.load(response.data);
+
+        // Extract the product title
+        const title = $('#productTitle').text().trim();
+
+        console.log({title});
+        
+    } catch (error: any) {
+        throw new Error(`Failed to scrape product: ${error.message}`)
+    }
+}
