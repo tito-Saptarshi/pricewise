891b4eb5bb150aef246c647648a92a4a25623231
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-02-25 23:58:38 +0530

    product page created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-
 import HeroCarousel from "@/components/HeroCarousel";
+import ProductCard from "@/components/ProductCard";
 import Searchbar from "@/components/Searchbar";
+import { getAllProducts } from "@/lib/actions";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className="px-6 md:px-20 py-4 ">
@@ -38,8 +41,8 @@ export default function Home() {
         <h2 className="section-text">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {["Apple Iphone 15", "Book", "Sneakers"].map((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
