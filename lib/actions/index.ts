5abb887d35b62013fda04bcb277674facf3225c9
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-02-26 21:21:51 +0530

    second fix

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -9,26 +9,24 @@ import { User } from "@/types";
 import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
-  if (!productUrl) return;
+  if(!productUrl) return;
 
   try {
     connectToDB();
 
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
-    if (!scrapedProduct) return;
+    if(!scrapedProduct) return;
 
     let product = scrapedProduct;
 
     const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
-    let newProduct = null;
-
-    if (existingProduct) {
+    if(existingProduct) {
       const updatedPriceHistory: any = [
         ...existingProduct.priceHistory,
-        { price: scrapedProduct.currentPrice },
-      ];
+        { price: scrapedProduct.currentPrice }
+      ]
 
       product = {
         ...scrapedProduct,
@@ -36,27 +34,18 @@ export async function scrapeAndStoreProduct(productUrl: string) {
         lowestPrice: getLowestPrice(updatedPriceHistory),
         highestPrice: getHighestPrice(updatedPriceHistory),
         averagePrice: getAveragePrice(updatedPriceHistory),
-      };
-      // updating the data in the database and if there's no data, creating new object of it and then uploading it
-      newProduct = await Product.findOneAndUpdate(
-        { url: product.url },
-        product,
-        { upsert: true, new: true }
-      );
-    } else {
-      const priceHistory = [];
-      priceHistory.push({  price: scrapedProduct.currentPrice });
-      product.priceHistory = priceHistory;
-
-      newProduct = new Product(product);
-      await newProduct.save();
+      }
     }
 
-    revalidatePath(`/products/${newProduct._id}`);
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
 
-    // console.log(newProduct);
+    revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
-    throw new Error(`Failed to create/update product: ${error.message}`);
+    throw new Error(`Failed to create/update product: ${error.message}`)
   }
 }
 
